e8225c702346ad6ea304ef4f88c5430572e92e27
Egor Solyanik <yokhor06@mail.ru>
2025-03-13 17:56:44 +0300

    Update tests for SE

--- checker.py
+++ checker.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2024 Egor Solyanik
+# SPDX-License-Identifier: MIT
+# 
+# Copyright (c) 2025 Egor Solyanik
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,12 +20,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 import subprocess
-import tqdm
+# import tqdm
 import argparse
-import datetime
-
+import os
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Floating point lab checker")
@@ -32,53 +32,91 @@ def parse_arguments():
     parser.add_argument('solution', nargs='?', help='Program to check')
     group.add_argument('-t', '--test', help='Test file to check')
     group.add_argument('--stress', action='store_true', help='Stress testing mode')
-    parser.add_argument('-r', '--rounding', default='1', help='Roundings to test')
+    parser.add_argument('-r', '--rounding', default='0123', help='Roundings to test')
     parser.add_argument('-o', '--operation', default='+-*/', help='Operations to test')
+    parser.add_argument('--special', action='store_true', help='High probability of zero, inf, den and nan')
 
     return parser.parse_args()
 
 
 def compile_solution(solution):
-    runnable = ""
-    if solution.endswith(".cpp"):
-        runnable = f'./{solution[:-4]}.out'
-        compile_cmd = f'clang++ {solution} -o {runnable}'
-    elif solution.endswith(".py"):
-        runnable = f'python3 {solution}'
-        compile_cmd = None
-    elif solution.endswith(".java"):
-        runnable = f'java {solution[:-5]}'
-        compile_cmd = f'javac {solution}'
-    elif solution.endswith(".out"):
-        runnable = f'./{solution}'
-        compile_cmd = None
+    if os.name == "nt":
+        # Windows
+        if solution.endswith(".cpp"):
+            exe = solution[:-4] + ".exe"
+            compile_cmd = f'clang++ {solution} -o {exe}'
+            runnable = exe
+        elif solution.endswith(".py"):
+            runnable = f'python {solution}'
+            compile_cmd = None
+        elif solution.endswith(".java"):
+            compile_cmd = f'javac {solution}'
+            runnable = f'java {solution[:-5]}'
+        elif solution.endswith(".exe"):
+            runnable = solution
+            compile_cmd = None
+        else:
+            raise ValueError(f"Unknown file extension for {solution}")
     else:
-        raise ValueError(f"Unknown file extension for {solution}")
+        # Linux / Unix
+        if solution.endswith(".cpp"):
+            runnable = f'./{solution[:-4]}.out'
+            compile_cmd = f'clang++ {solution} -o {solution[:-4]}.out'
+        elif solution.endswith(".py"):
+            runnable = f'python3 {solution}'
+            compile_cmd = None
+        elif solution.endswith(".java"):
+            compile_cmd = f'javac {solution}'
+            runnable = f'java {solution[:-5]}'
+        elif solution.endswith(".out"):
+            runnable = f'./{solution}'
+            compile_cmd = None
+        else:
+            raise ValueError(f"Unknown file extension for {solution}")
     if compile_cmd:
         subprocess.run(compile_cmd, shell=True)
-
     return runnable
 
 
+def validate_value(res):
+    if res == "0x0p+0":
+        res = "0x0.000000p+0"
+    elif res == "-0x0p+0":
+        res = "-0x0.000000p+0"
+    elif res.startswith("0x1p"):
+        res = "0x1.000000p" + res[res.index('p') + 1:]
+    elif res.startswith("-0x1p"):
+        res = "-0x1.000000p" + res[res.index('p') + 1:]
+    elif res == "-nan":
+        res = "nan"
+    elif ("nan" not in res) and ("inf" not in res):
+        l = len(res[res.index('.') + 1: res.index('p')])
+        if l < 6:
+            res = res[0:res.index('p')] + "0"*(6-l) + res[res.index('p'):]
+    return res
+
+
 def test(test_file, runnable):
-    # Run tests
     with open(test_file, "r") as f:
         tests = [line.strip() for line in f.readlines()]
 
     i = 1
-    for test in tqdm.tqdm(tests, desc=f'Running {test_file}'):
+    for test in tests:
         parts = test.split("#")
         cmd = f'{runnable} {parts[0].strip()}'
-        correct_output = parts[1].strip()
+        
+        # value, hex = parts[-1].strip().split(" ")
+        correct_output = parts[-1].strip()
+        # value = validate_value(value)
+        # correct_output = value + " " + hex
 
-        # Run the test
         output = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         actual_output = output.stdout.decode("utf-8").strip()
 
-        # Check if the test passed
         if actual_output != correct_output:
             if actual_output == "skip": continue
-            print(f"Test number {i} failed: {test}\n   Your output: {actual_output}\nCorrect output: {correct_output}")
+            print(f"Test number {i} failed: \033[91m{parts[0].strip()}\033[0m\
+                  \n   Your output: {actual_output}\nCorrect output: {correct_output}")
             break
         i += 1
     else:
@@ -88,7 +126,7 @@ def test(test_file, runnable):
 def log_error(test, solution_output, answer):
     print()
     print("Failed test")
-    print(f'\t {test}')
+    print(f'\t \033[91m{test.split("#")[0]}\033[0m')
     print("Your output: ")
     print(f'\t {solution_output}')
     print("Correct output: ")
@@ -96,40 +134,44 @@ def log_error(test, solution_output, answer):
     print()
 
 
-def stress(runnable, rounding, operation):
+def stress(runnable, rounding, operation, special):
+    special = 1 if special else 0
     with open("stress_log.txt", "w") as f:
         f.write("")
     i = 1
+
+    # Для Windows используем gen.exe, для Linux — gen.out
+    gen_cmd = "gen.exe" if os.name == "nt" else "./gen.out"
+
     while True:
-        test_cmd = f'./gen.out {rounding} "{operation}" 1'
+        test_cmd = f'{gen_cmd} {rounding} "{operation}" 1 {special}'
         test = subprocess.run(test_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode("utf-8").strip()
         parts = test.split("#")
 
-        answer = parts[1].strip()
-
-        solution_cmd = f'./{runnable} {parts[0].strip()}'
+        value, hex_val = parts[-1].strip().split(" ")
+        value = validate_value(value)
+        answer = value + " " + hex_val
+        
+        solution_cmd = f'{runnable} {parts[0].strip()}'
         solution_output = subprocess.run(solution_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode("utf-8").strip()
         if solution_output != answer:
-            if solution_output == "skip":
-                continue
             with open("stress_log.txt", "a") as f:
-                f.write(test)
-                f.write("\n")
+                f.write(test + "\n")
             log_error(test, solution_output, answer)
+            break
         else:
-            print(f'\rTests successfuly passed: {i}', end='')
+            print(f'\rTests successfully passed: {i}', end='')
         i += 1
 
 
 def main():
-    print('\033[91m' + '***** By Egor Solyanik M3139: https://t.me/yokhor *****' + '\033[0m\n')
-    print(f'Started testing on {datetime.datetime.now().strftime("%d.%m")} at {datetime.datetime.now().strftime("%H:%M")}')
+    print('\033[91m' + '***** By Egor Solyanik M3139 *****' + '\033[0m\n')
+    
     args = parse_arguments()
-
     runnable = compile_solution(args.solution)
-
+    
     if args.stress:
-        stress(runnable, args.rounding, args.operation)
+        stress(runnable, args.rounding, args.operation, args.special)
     elif args.test:
         test(args.test, runnable)
 
---
+++ solid.cpp
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+// 
+// Copyright (c) 2025 Egor Solyanik
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#include <cfenv>
+#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <cstdint>
+#include <cmath>
+
+using namespace std;
+
+void setRounding(char c) {
+    switch (c) {
+        case '0':
+            fesetround(FE_TOWARDZERO);
+            break;
+        case '1':
+            fesetround(FE_TONEAREST);
+            break;
+        case '2':
+            fesetround(FE_UPWARD);
+            break;
+        case '3':
+            fesetround(FE_DOWNWARD);
+            break;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    setRounding(argv[2][0]);
+    char operation = argv[3][0];
+
+    union {
+        float f;
+        uint32_t i;
+    } u_A, u_B;
+
+    u_A.i = stoul(argv[4], nullptr, 16);
+    u_B.i = stoul(argv[5], nullptr, 16);
+
+    float A = u_A.f;
+    float B = u_B.f;
+
+    float result = 0;
+    switch (operation) {
+        case '+':
+            result = A + B;
+            break;
+        case '-':
+            result = A - B;
+            break;
+        case '*':
+            result = A * B;
+            break;
+        case '/':
+            result = A / B;
+            break;
+    }
+
+    // Print the result as a hexadecimal value
+    union {
+        float f;
+        uint32_t i;
+    } u_result;
+    u_result.f = result;
+
+    cout << hex << showbase << hexfloat << result << " ";
+    cout << "0x" << noshowbase << uppercase << setw(8) << setfill('0');
+    cout << u_result.i << endl;
+
+    return 0;
+}
